df51115fd67b63f0944975f1c01820eb534df5b3
VaticanCameos <chakrabortypritish@gmail.com>
2015-07-21 00:13:49 +0530

    Refactored API_HOST into Angular module constant API

--- app/js/main/providers/main-services.js
+++ app/js/main/providers/main-services.js
@@ -1,28 +1,26 @@
 (function() {
   'use strict';
-  var API_HOST, app;
+  var app;
 
-  app = angular.module("MainApp");
+  app = angular.module('MainApp');
 
-  API_HOST = "https://chutter-api.herokuapp.com/api/v1";
-
-  app.factory("NetworkSubscriptionResource", [
-    "$resource", "Page", function($resource, Page) {
-      return $resource(API_HOST + "/network_subscriptions/:id", {
-        id: "@id"
+  app.factory('NetworkSubscriptionResource', [
+    '$resource', 'Page', 'API', function($resource, Page, API) {
+      return $resource(API.makeURL('/network_subscriptions/:id'), {
+        id: '@id'
       }, {
-        "delete": {
-          method: "DELETE",
+        'delete': {
+          method: 'DELETE',
           isArray: true
         },
         save: {
-          method: "POST",
+          method: 'POST',
           isArray: true
         },
         query: {
           isArray: true,
           interceptor: {
-            "response": function(response) {
+            'response': function(response) {
               Page.networkSubscriptions = response.data;
               Page.communitySubscriptions = [];
               _.each(Page.networkSubscriptions, function(networkSubscription) {
@@ -38,144 +36,146 @@
     }
   ]);
 
-  app.factory("CommunitySubscriptionResource", [
-    "$resource", "Page", function($resource, Page) {
-      return $resource(API_HOST + "/community_subscriptions/:id", {
-        id: "@id"
+  app.factory('CommunitySubscriptionResource', [
+    '$resource', 'Page', 'API', function($resource, Page, API) {
+      return $resource(API.makeURL('/community_subscriptions/:id'), {
+        id: '@id'
       }, {
-        "delete": {
-          method: "DELETE",
+        'delete': {
+          method: 'DELETE',
           isArray: true
         },
         save: {
-          method: "POST",
+          method: 'POST',
           isArray: true
         }
       });
     }
   ]);
 
-  app.factory("UserResource", [
-    "$resource", "Page", function($resource, Page) {
-      return $resource(API_HOST + "/users/:id", {
-        id: "@id"
+  app.factory('UserResource', [
+    '$resource', 'Page', 'API', function($resource, Page, API) {
+      return $resource(API.makeURL('/users/:id'), {
+        id: '@id'
       });
     }
   ]);
 
-  app.factory("ExternalServicesResource", [
-    "$resource", "Page", function($resource, Page) {
-      return $resource(API_HOST + "/external_services/:id", {
-        id: "@id"
+  app.factory('ExternalServicesResource', [
+    '$resource', 'Page', 'API', function($resource, Page, API) {
+      return $resource(API.makeURL('/external_services/:id'), {
+        id: '@id'
       }, {
-        "search": {
-          url: API_HOST + "/external_services/search_music",
-          method: "POST",
+        'search': {
+          url: API.makeURL('/external_services/search_music'),
+          method: 'POST',
           isArray: true
         }
       });
     }
   ]);
 
-  app.factory("MediaResource", [
-    "$resource", "Page", function($resource, Page) {
-      return $resource(API_HOST + "/media/:id", {
-        id: "@id"
+  app.factory('MediaResource', [
+    '$resource', 'Page', 'API', function($resource, Page, API) {
+      return $resource(API.makeURL('/media/:id'), {
+        id: '@id'
       }, {
-        "resolve": {
-          url: API_HOST + "/media/resolve",
-          method: "POST",
+        'resolve': {
+          url: API.makeURL('/media/resolve'),
+          method: 'POST',
           isArray: false
         }
       });
     }
   ]);
 
-  app.factory("CommunityResource", [
-    "$resource", "Page", function($resource, Page) {
-      return $resource(API_HOST + "/communities/:id", {
-        id: "@id"
+  app.factory('CommunityResource', [
+    '$resource', 'Page', 'API', function($resource, Page, API) {
+      return $resource(API.makeURL('/communities/:id'), {
+        id: '@id'
       }, {
         show: {
-          method: "GET",
-          url: API_HOST + "/communities/:id",
+          method: 'GET',
+          url: API.makeURL('/communities/:id'),
           interceptor: {
-            "response": function(response) {
+            'response': function(response) {
               return Page.community = response.data;
             }
           }
         },
         posts: {
-          method: "GET",
+          method: 'GET',
           isArray: true,
-          url: API_HOST + "/communities/:id/posts"
+          url: API.makeURL('/communities/:id/posts')
         }
       });
     }
   ]);
 
-  app.factory("CommentResource", [
-    "$resource", "Page", function($resource, Page) {
-      return $resource(API_HOST + "/comments/:id", {
-        id: "@id"
+  app.factory('CommentResource', [
+    '$resource', 'Page', 'API', function($resource, Page, API) {
+      return $resource(API.makeURL('/comments/:id'), {
+        id: '@id'
       });
     }
   ]);
 
-  app.factory("PostResource", [
-    "$resource", "Page", function($resource, Page) {
-      return $resource(API_HOST + "/posts/:id", {
-        id: "@id"
+  app.factory('PostResource', [
+    '$resource', 'Page', 'API', function($resource, Page, API) {
+      return $resource(API.makeURL('/posts/:id'), {
+        id: '@id'
       }, {
         query: {
           isArray: true,
           interceptor: {
-            "response": function(response) {
+            'response': function(response) {
               return Page.posts = response.data;
             }
           }
         },
         comments: {
           transformRequest: [],
-          url: API_HOST + "/posts/:id/comments",
+          url: API.makeURL('/posts/:id/comments'),
           isArray: true
         },
         vote: {
-          method: "PUT",
-          url: API_HOST + "/posts/:id/vote"
+          method: 'PUT',
+          url: API.makeURL('/posts/:id/vote')
         }
       });
     }
   ]);
 
-  app.factory("NetworkResource", [
-    "$resource", "Page", function($resource, Page) {
-      return $resource(API_HOST + "/networks/:id", {
-        id: "@id"
+  app.factory('NetworkResource', [
+    '$resource', 'Page', 'API', function($resource, Page, API) {
+      return $resource(API.makeURL('/networks/:id'), {
+        id: '@id'
       }, {
         show: {
-          method: "GET",
-          url: API_HOST + "/networks/:id",
+          method: 'GET',
+          url: API.makeURL('/networks/:id'),
           interceptor: {
-            "response": function(response) {
+            'response': function(response) {
               return Page.network = response.data;
             }
           }
         },
         posts: {
-          method: "GET",
+          method: 'GET',
           isArray: true,
-          url: API_HOST + "/networks/:id/posts"
+          url: API.makeURL('/networks/:id/posts')
         },
         list: {
-          method: "GET",
-          url: API_HOST + "/networks/list",
+          method: 'GET',
+          url: API.makeURL('/networks/list'),
           isArray: true,
           transformResponse: function(response) {
             var data;
+            data = void 0;
             data = angular.fromJson(response);
             _.each(data, function(item) {
               var sub;
+              sub = void 0;
               sub = _.findWhere(Page.networkSubscriptions, {
                 network_id: item.id
               });
@@ -190,14 +190,16 @@
           }
         },
         communities: {
-          method: "get",
-          url: API_HOST + "/networks/:id/communities",
+          method: 'get',
+          url: API.makeURL('/networks/:id/communities'),
           isArray: true,
           transformResponse: function(response) {
             var data;
+            data = void 0;
             data = angular.fromJson(response);
             _.each(data, function(item) {
               var sub;
+              sub = void 0;
               sub = _.findWhere(Page.communitySubscriptions, {
                 community_id: item.id
               });
@@ -216,34 +218,36 @@
     }
   ]);
 
-  app.factory("audio", [
-    "$document", function($document) {
+  app.factory('audio', [
+    '$document', function($document) {
       var audio;
       audio = void 0;
-      audio = $document[0].createElement("audio");
+      audio = void 0;
+      audio = $document[0].createElement('audio');
       return audio;
     }
   ]);
 
-  app.factory("preview", [
-    "$document", function($document) {
+  app.factory('preview', [
+    '$document', function($document) {
       var audio;
       audio = void 0;
-      audio = $document[0].createElement("audio");
+      audio = void 0;
+      audio = $document[0].createElement('audio');
       return audio;
     }
   ]);
 
-  app.factory("playerSvc", [
-    "$http", "$rootScope", function($http, $rootScope) {
+  app.factory('playerSvc', [
+    '$http', '$rootScope', function($http, $rootScope) {
       return {
         incrPlays: function(songId) {
-          return $http.post("/incrPlays", {
+          return $http.post('/incrPlays', {
             songId: songId
           });
         },
         getSong: function(track) {
-          return $http.get("/getSong?track=" + track);
+          return $http.get('/getSong?track=' + track);
         }
       };
     }
